651634626d80866866017e5849b5dd1e67bb624f
Ngan Pham <ngan@users.noreply.github.com>
2022-04-18 00:11:43 -0700

    Release 0.5.0

--- lib/subbundle.rb
+++ lib/subbundle.rb
@@ -1,78 +1,22 @@
 # frozen_string_literal: true
 
 require_relative "subbundle/version"
+require "bundler"
 
 module Subbundle
-  def self.setup(*gems, name: nil)
-    require "bundler"
-    require "digest/md5"
+  def self.setup(*gems)
+    # Don't write to the lockfile.
+    Bundler::Definition.no_lock = true
 
-    name ||= File.basename($PROGRAM_NAME)
-    gems = gems.map(&:to_s)
+    Bundler.definition.tap do |definition|
+      definition.dependencies.keep_if { |dependency| gems.include?(dependency.name) }
+      definition.locked_deps.keep_if { |name, _| gems.include?(name) }
 
-    digest = Digest::MD5.new
-    digest << Bundler.default_lockfile.to_s
-    digest.file(Bundler.default_lockfile)
-    gems.sort.each { |dep| digest << dep }
-
-    subbundle_lockfile_path = Bundler.app_config_path.join("subbundles/#{name}.#{digest.hexdigest}.lock")
-
-    if subbundle_lockfile_path.exist?
-      return if setup_from_subbundle(gems, subbundle_lockfile_path)
+      # Overrides the specs
+      spec_set = Bundler::SpecSet.new(definition.instance_variable_get(:@locked_specs).for(definition.dependencies, false, true))
+      definition.instance_variable_set(:@locked_specs, spec_set)
     end
 
-    default_lockfile = Bundler::LockfileParser.new(Bundler.read_file(Bundler.default_lockfile))
-    required_dependencies = default_lockfile.dependencies.slice(*gems).values
-    spec_set = Bundler::SpecSet.new(default_lockfile.specs)
-    specs = spec_set.for(required_dependencies, false, true)
-    dependencies = default_lockfile.dependencies.slice(*specs.map(&:name)).values
-    source_list = build_source_list(specs.map(&:source).uniq)
-    definition = Bundler::Definition.new(Bundler.default_lockfile, dependencies, source_list, {})
-    subbundle_lockfile_path.dirname.mkpath
-    subbundle_lockfile_path.write(Bundler.ui.silence { definition.to_lock })
-
-    # Delete old caches.
-    Bundler.app_config_path.glob("subbundles/#{name}.*.lock").each do |path|
-      path.delete if path != subbundle_lockfile_path
-    end
-
-    setup_from_subbundle(gems, subbundle_lockfile_path)
-  end
-
-  def self.setup_from_subbundle(gems, subbundle_lockfile_path)
-    subbundle_lockfile = Bundler::LockfileParser.new(Bundler.read_file(subbundle_lockfile_path))
-    dependencies = subbundle_lockfile.dependencies.slice(*gems).values
-    source_list = build_source_list(subbundle_lockfile.sources)
-    definition = Bundler::Definition.new(subbundle_lockfile_path, dependencies, source_list, {})
-    if definition.nothing_changed?
-      setup_runtime(definition)
-      true
-    else
-      subbundle_lockfile_path.delete
-      false
-    end
-  end
-
-  def self.build_source_list(sources)
-    sources.compact.each_with_object(Bundler::SourceList.new) do |source, source_list|
-      case source
-      when Bundler::Source::Git
-        source_list.add_git_source(source.options)
-      when Bundler::Source::Path, Bundler::Source::Gemspec
-        source_list.add_path_source(source.options)
-      when Bundler::Source::Rubygems
-        next if source.remotes.empty?
-        source_list.add_rubygems_source(source.options)
-      else
-        raise "unhandled: #{source.inspect}"
-      end
-    end
-  end
-
-  def self.setup_runtime(definition)
-    runtime = Bundler::Runtime.new(Bundler.root, definition)
-    # This is not ideal, let's revisit later.
-    Bundler.instance_variable_set(:@load, runtime)
-    Bundler.ui.silence { runtime.setup }
+    Bundler.require
   end
 end
